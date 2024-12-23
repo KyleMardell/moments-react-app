6745f599b15dd1e1b630cbc405e8d30d6c330bfb
Kyle Mardell <kyleslufc@gmail.com>
2024-12-23 22:38:00 +0000

    Add initial posts pages.

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -5,15 +5,19 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+
+import Asset from "../../components/Asset"
 
 import Upload from "../../assets/upload.png";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import Asset from "../../components/Assets";
-import { Image } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
+
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostCreateForm() {
   const [errors, setErrors] = useState({});
@@ -23,7 +27,6 @@ function PostCreateForm() {
     content: "",
     image: "",
   });
-
   const { title, content, image } = postData;
 
   const imageInput = useRef(null);
@@ -86,8 +89,8 @@ function PostCreateForm() {
         <Form.Label>Content</Form.Label>
         <Form.Control
           as="textarea"
-          name="content"
           rows={6}
+          name="content"
           value={content}
           onChange={handleChange}
         />
@@ -100,12 +103,11 @@ function PostCreateForm() {
 
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => history.goBack()}>
+        onClick={() => history.goBack()}
+      >
         cancel
       </Button>
-      <Button
-        className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        type="submit">
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
         create
       </Button>
     </div>
@@ -116,21 +118,19 @@ function PostCreateForm() {
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
-            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}>
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
             <Form.Group className="text-center">
               {image ? (
                 <>
                   <figure>
-                    <Image
-                      className={appStyles.Image}
-                      src={image}
-                      rounded
-                    />
+                    <Image className={appStyles.Image} src={image} rounded />
                   </figure>
                   <div>
                     <Form.Label
                       className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
-                      htmlFor="image-upload">
+                      htmlFor="image-upload"
+                    >
                       Change the image
                     </Form.Label>
                   </div>
@@ -138,13 +138,15 @@ function PostCreateForm() {
               ) : (
                 <Form.Label
                   className="d-flex justify-content-center"
-                  htmlFor="image-upload">
+                  htmlFor="image-upload"
+                >
                   <Asset
                     src={Upload}
-                    message="Click or tap to upload an image."
+                    message="Click or tap to upload an image"
                   />
                 </Form.Label>
               )}
+
               <Form.File
                 id="image-upload"
                 accept="image/*"
@@ -162,9 +164,7 @@ function PostCreateForm() {
           </Container>
         </Col>
         <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-          <Container className={appStyles.Content}>
-            {textFields}
-          </Container>
+          <Container className={appStyles.Content}>{textFields}</Container>
         </Col>
       </Row>
     </Form>
